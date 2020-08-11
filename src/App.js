8993b81488e880326d69bd1298c2294149ae77b3
Pavel <alexgromik@gmail.com>
2020-08-11 21:56:50 +0300

    feat: add audio player and basic styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit src/App.js and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="line"></div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Main />
+                <h1 className='test'>Test div text</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
