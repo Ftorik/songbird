4c1ff5b4c9e13d1cc3cd213b9e439036bc7d4296
Pavel <alexgromik@gmail.com>
2020-08-13 23:16:24 +0300

    feat: add additional styles

--- src/components/App.js
+++ src/components/App.js
@@ -20,10 +20,10 @@ const App = () => {
 
     return (
         <>
-            <Row style={{backgroundColor:'#ffffff30'}}>
-                <Col style={{backgroundColor:'#0000ff40'}} offset={2} span={20}>
+            <Row style={{backgroundColor:'#ffffff30',display:'flex', flexDirection:'column',alignItems:'center'}}>
+                <Col style={{ width:'75%' }}>
                     {/*Header*/}
-                    <Row style={{backgroundColor:'#ff00ff40', paddingBottom: 20}}>
+                    <Row style={{backgroundColor:'#17288e', marginBottom: 25, marginTop:20}}>
                         {/*Header Logo left*/}
                         <Col span={12}>
                             <img alt='test_image' src='https://media.gettyimages.com/photos/woman-lifts-her-arms-in-victory-mount-everest-national-park-picture-id507910624?s=612x612' width={200} height={155}/>
@@ -36,34 +36,47 @@ const App = () => {
                         </Col>
                     </Row>
                     {/*Breadcrumbs*/}
-                    <Row style={{backgroundColor:'#ffffff40'}}>
-                        <Col span={24}>BREADCRUMBS</Col>
+                    <Row style={{textAlign:'center',marginBottom: 25,  borderWidth:1, borderRadius:5, justifyContent:'space-around',fontWeight:500,backgroundColor:'white',userSelect:'none'}}>
+                        <Col  style={{backgroundColor:'yellow',display:'block', paddingLeft:25,paddingRight:25 }}>TvShow</Col>
+                        <Col  style={{backgroundColor:'lightgreen',display:'block', paddingLeft:25,paddingRight:25 }}>Series</Col>
+                        <Col  style={{backgroundColor:'yellow',display:'block', paddingLeft:25,paddingRight:25}}>Movies</Col>
+                        <Col  style={{backgroundColor:'lightgreen',display:'block', paddingLeft:25,paddingRight:25 }}>Cartoons</Col>
+                        <Col  style={{backgroundColor:'yellow',display:'block', paddingLeft:25,paddingRight:25 }}>Anime</Col>
+                        <Col  style={{backgroundColor:'lightgreen',display:'block',paddingLeft:25,paddingRight:25, }}>Games</Col>
                     </Row>
                     {/*Question: photo and audio player*/}
-                    <Row style={{backgroundColor:'#ff0f0f40'}}>
-                        <Col span={24}>PHOTO + PLAYER</Col>
+                    <Row style={{backgroundColor:'#ff0f0f40',marginBottom: 25,display:"flex"}}>
+                        <Col className='row-items' style={{backgroundColor:'lightgreen',flex:1,display:"flex" }}>
+                            <div className='pic_player' style={{flex:0,display:"block"}}>
+                                <img alt='test_image' src='https://media.gettyimages.com/photos/woman-lifts-her-arms-in-victory-mount-everest-national-park-picture-id507910624?s=612x612' width={200} height={155}/>
+                            </div>
+                            <div style={{backgroundColor:'lightblue',display:'block',flex:1}}>Audioplayer + coded word</div>
+                        </Col>
+                    </Row>
+                    {/*Button - next*/}
+                    <Row style={{marginBottom: 25}}>
+                        <Col span={24} style={{display:'flex', justifyContent:'center'}}>
+                            {/*active button*/}
+                            <button style={{display:'block', width:'100%', border:'1px solid', borderColor:'#fe1f4f', borderRadius:10,boxShadow: '2px 2px 5px #fe1f4f', backgroundColor:'#230f1b',color:'#fe1f4f'}} type="button">
+                            {/*inactive button*/}
+                            {/*<button style={{display:'block', width:'100%', border:'none', borderRadius:10, backgroundColor:'#2b2b2c',color:'gray'}} type="button">*/}
+                                NEXT
+                            </button>
+                        </Col>
                     </Row>
                     {/*Body*/}
-                    <Row style={{backgroundColor:'#f0ff0f40'}}>
+                    <Row className='row-items' style={{backgroundColor:'#f0ff0f40',display:'flex', justifyContent:'space-between',}}>
                         {/*Body items list - left*/}
-                        <Col span={12}>
+                        <Col className='items' style={{flex:1,}}>
                             <div style={{backgroundColor:'#f0ff0f40'}}>
                                 LEFT
                             </div>
                         </Col>
                         {/*Body active item list - right*/}
-                        <Col span={12}>
-                            <div style={{backgroundColor:'#ffffff40'}}>
+                        <Col style={{flex:2}} >
+                            <div style={{backgroundColor:'#ffffff40',}}>
                                 RIGHT
                             </div>
-                        </Col>
-                    </Row>
-                    {/*Button - next*/}
-                    <Row style={{backgroundColor:'#00ffff40'}}>
-                        <Col span={24} style={{display:'flex', justifyContent:'center'}}>
-                            <button style={{display:'block', width:'100%', border:'none', borderRadius:5, backgroundColor:'green'}} type="button">
-                                NEXT
-                            </button>
                         </Col>
                     </Row>
                 </Col>
