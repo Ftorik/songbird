7d90d5b76ffd6794a4710bb469341dc20084bd76
Pavel <alexgromik@gmail.com>
2020-08-16 00:21:50 +0300

    feat: add more styles, add sounds

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,34 @@
 import React from 'react';
+import {Row, Col,} from 'antd';
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
 import './App.css';
-import {Row, Col, List} from 'antd';
-// import logo from '../logo.svg';
 
-// function App() {
-//     return (
-//         <div className="App">
-//             <div className="line"></div>
-//             <header className="App-header">
-//                 <img src={logo} className="App-logo" alt="logo" />
-//                 <h1 className='line'>Hello World</h1>
-//                 <h1 className='test'>Test div text</h1>
-//             </header>
-//         </div>
-//     );
-// }
+import logo from '../images/LOGO.png'
+import unsolved from '../images/unknow_pic.jpg'
+import sounds from '../utils/sounds';
 
 const App = () => {
     const data = [
-        'Racing car sprays burning fuel into crowd.',
-        'Japanese princess to wed commoner.',
-        'Australian walks 100km after outback crash.',
-        'Man charged over missing wedding girl.',
-        'Los Angeles battles huge wildfires.',
+        'Pimp my ride',
+        'Fort Boyard',
+        'Зов джунглей',
+        'Сам себе режиссёр',
+        'X Factor',
+        'В мире животных'
     ];
     return (
         <>
             <Row style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <Col style={{width: '75%'}}>
+                <Col className='container'>
                     {/*Header*/}
-                    <Row style={{marginBottom: 25, marginTop: 20}}>
+                    <Row style={{marginTop: 5}}>
                         {/*Header Logo left*/}
                         <Col span={12}>
                             <img alt='test_image'
-                                 src='https://media.gettyimages.com/photos/woman-lifts-her-arms-in-victory-mount-everest-national-park-picture-id507910624?s=612x612'
-                                 width={200} height={155} />
+                                 src={logo}
+                                 style={{borderTopLeftRadius: 20, marginBottom: -18, zIndex: -1, position: "relative"}}
+                                 width={120} height={100} />
                         </Col>
                         {/*Header Score right*/}
                         <Col span={12} style={{display: "flex", justifyContent: "flex-end", alignItems: 'flex-end'}}>
@@ -45,10 +39,11 @@ const App = () => {
                     </Row>
                     {/*Breadcrumbs*/}
                     <Row style={{
+                        zIndex: 99,
                         lineHeight: 2,
                         textAlign: 'center',
                         color: '#fe1f4f',
-                        marginBottom: 25,
+                        marginBottom: 10,
                         border: '1px solid',
                         borderRadius: 10,
                         justifyContent: 'space-around',
@@ -56,16 +51,22 @@ const App = () => {
                         backgroundColor: '#230f1b',
                         userSelect: 'none'
                     }}>
-                        <Col style={{display: 'block', paddingLeft: 24, paddingRight: 12, color: 'white'}}>TvShow</Col>
-                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12}}>Series</Col>
-                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12}}>Movies</Col>
-                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12}}>Cartoons</Col>
-                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12}}>Anime</Col>
-                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 24,}}>Games</Col>
+                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12, color: '#fff'}}>TvShow</Col>
+                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12, color: '#fff'}}>Series</Col>
+                        <Col
+                            style={{display: 'block', paddingLeft: 12, paddingRight: 12, color: '#fe1f4f'}}>Movies</Col>
+                        <Col style={{
+                            display: 'block',
+                            paddingLeft: 12,
+                            paddingRight: 12,
+                            color: '#fff'
+                        }}>Cartoons</Col>
+                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12, color: '#fff'}}>Anime</Col>
+                        <Col style={{display: 'block', paddingLeft: 12, paddingRight: 12, color: '#fff'}}>Games</Col>
                     </Row>
                     {/*Question: photo and audio player*/}
                     <Row style={{
-                        marginBottom: 25,
+                        marginBottom: 10,
                         display: "flex",
                         color: '#fe1f4f',
                         border: '1px solid',
@@ -73,72 +74,117 @@ const App = () => {
                         backgroundColor: '#230f1b'
                     }}>
                         <Col className='row-items' style={{flex: 1, display: "flex"}}>
-                            <div className='pic_player' style={{flex: 0, display: "block"}}>
+                            <div className='pic_player' style={{flex: 1,}}>
                                 <img alt='test_image'
-                                     src='https://media.gettyimages.com/photos/woman-lifts-her-arms-in-victory-mount-everest-national-park-picture-id507910624?s=612x612'
+                                     style={{borderRadius: 10}}
+                                     src={unsolved}
                                      width={200} height={155} />
                             </div>
-                            <div style={{display: 'block', flex: 1, color: '#fff'}}>
-                                <div style={{width: '90%'}}>
-                                    <div style={{alignItems: "center", flex: 1}}>****** Word</div>
-                                    <div style={{border: '1px solid', color: '#3c3946'}} />
-                                    <p>Audioplayer + coded word</p>
+                            <div style={{flex: 4, display: 'flex', color: '#fff',}}>
+                                <div style={{flex: 1, display: 'flex', flexDirection: 'column',}}>
+                                    <div className='coded_word'>**********</div>
+                                    <div className='player_container'>
+                                        <div style={{border: '1px solid', backgroundColor: '#fff',}} />
+                                        <div style={{marginTop: 20,}}>
+                                            <AudioPlayer src={sounds.etc.wrong}
+                                                         volume={0.33}
+                                                         style={{flex: 1}}
+                                                         customAdditionalControls={[]}
+                                                         showJumpControls={false}
+                                            />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </Col>
                     </Row>
                     {/*Button - next*/}
-                    <Row style={{marginBottom: 25}}>
+                    <Row style={{marginBottom: 10}}>
                         <Col span={24} style={{display: 'flex', justifyContent: 'center'}}>
                             {/*active button*/}
-                            <button style={{
-                                display: 'block',
-                                width: '100%',
-                                border: '1px solid',
-                                borderColor: '#fe1f4f',
-                                borderRadius: 10,
-                                backgroundColor: '#230f1b',
-                                color: '#fe1f4f',
-                                boxShadow: '2px 2px 5px #fe1f4f',
-                            }} type="button">
-                                {/*inactive button*/}
-                                {/*<button style={{display:'block', width:'100%', border:'1px solid', borderRadius:10, backgroundColor:'#2b2b2c',color:'gray'}} type="button">*/}
-                                NEXT
-                            </button>
+                            {/*<div className='button'>*/}
+                            {/*inactive button*/}
+                            <div className='button_disabled'>
+                                Следующий уровень
+                            </div>
                         </Col>
                     </Row>
                     {/*Body*/}
                     <Row className='row-items' style={{display: 'flex', justifyContent: 'space-between',}}>
                         {/*Body items list - left*/}
-                        <Col className='items' style={{flex: 1,}}>
-                            <div style={{
-                                backgroundColor: '#230f1b',
-                                color: '#fe1f4f',
-                                border: '1px solid',
-                                borderColor: '#fe1f4f',
-                                borderRadius: 10,
-                            }}>
-                                LEFT
-                                <List
-                                    size="small"
-                                    dataSource={data}
-                                    renderItem={item =>
-                                        <List.Item style={{borderColor: '#fe1f4f',color:'#fff'}}>
-                                            {item}
-                                        </List.Item>}
-                                />
+                        <Col className='items items_left'>
+                            <div className='container_items_left'>
+                                {data.map(item => {
+                                    return <div key={item} className='item'>
+                                        <span style={{
+                                            borderRadius: '50%',
+                                            height: 10,
+                                            width: 10,
+                                            backgroundColor: '#6CFD1F',
+                                            display: 'inline-block'
+                                        }} />
+                                        <span style={{
+                                            borderRadius: '50%',
+                                            height: 10,
+                                            width: 10,
+                                            backgroundColor: '#fe1f4f',
+                                            display: 'inline-block'
+                                        }} />
+                                        <span style={{
+                                            borderRadius: '10px',
+                                            height: 10,
+                                            width: 10,
+                                            border: '2px solid #9e9e9e',
+                                            backgroundColor: '#2b2b2c',
+                                            display: 'inline-block',
+                                            marginRight: 5
+                                        }} />
+                                        {item}
+                                    </div>
+                                })
+                                }
                             </div>
                         </Col>
                         {/*Body active item list - right*/}
                         <Col style={{flex: 2}}>
-                            <div style={{
+                            <div className='row-items2' style={{
                                 backgroundColor: '#230f1b',
                                 color: '#fe1f4f',
                                 border: '1px solid',
                                 borderColor: '#fe1f4f',
                                 borderRadius: 10,
+                                flex: 1,
+                                display: "flex"
                             }}>
-                                RIGHT
+                                <div className='pic_player2' style={{flex: 1,}}>
+                                    <img alt='test_image'
+                                         style={{borderRadius: 10}}
+                                         src='https://media.gettyimages.com/photos/woman-lifts-her-arms-in-victory-mount-everest-national-park-picture-id507910624?s=612x612'
+                                         width={200} height={155} />
+                                </div>
+                                <div style={{flex: 4, display: 'flex', color: '#fff',}}>
+                                    <div style={{flex: 1, display: 'flex', flexDirection: 'column',}}>
+                                        <div className='coded_word2'>SLOVO</div>
+                                        <div className='player_container2'>
+                                            <div style={{border: '1px solid', backgroundColor: '#fff',}} />
+                                            <div className='coded_word2 word_info'>TSDADSAAS</div>
+                                            <div style={{border: '1px solid', backgroundColor: '#fff',}} />
+                                            <div style={{marginTop: 20,}}>
+                                                <AudioPlayer src={sounds.movies.ghostbusters}
+                                                             style={{flex: 1}}
+                                                             volume={0.33}
+                                                             customAdditionalControls={[]}
+                                                             showJumpControls={false}
+
+                                                />
+                                                <p className='word_info'>Козодой – неприметная птица, известная
+                                                    благодаря своему голосу. Песня козодоя звучит как монотонная трель
+                                                    похожая на тарахтение мотоцикла. Такое дребезжание слышно от заката
+                                                    до рассвета, его тональность, частота и громкость изменяются.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </Col>
                     </Row>
---
+++ src/utils/sounds.js
@@ -0,0 +1,104 @@
+import alchemist from '../sounds/anime/alchemist.mp3';
+import beck from '../sounds/anime/beck.mp3';
+import fate from '../sounds/anime/fate.mp3';
+import grandblue from '../sounds/anime/grandblue.mp3';
+import souleater from '../sounds/anime/souleater.mp3';
+import steinsgate from '../sounds/anime/steinsgate.mp3';
+
+import alladin from '../sounds/cartoons/alladin.mp3'
+import ducktales from '../sounds/cartoons/ducktales.mp3'
+import frozen from '../sounds/cartoons/frozen.mp3'
+import mulan from '../sounds/cartoons/mulan.mp3'
+import chipanddale from '../sounds/cartoons/rescuerangers.mp3'
+import tmnt from '../sounds/cartoons/tmnt.mp3'
+
+import ac2 from '../sounds/games/ac2.mp3'
+import battlefield from '../sounds/games/battlefield.mp3'
+import divinity from '../sounds/games/divinity.mp3'
+import fallout4 from '../sounds/games/fallout4.mp3'
+import morrowind from '../sounds/games/morrowind.mp3'
+import undertale from '../sounds/games/undertale.mp3'
+
+import badboys from '../sounds/movies/badboys.mp3'
+import ghostbusters from '../sounds/movies/ghostbusters.mp3'
+import lotr from '../sounds/movies/lotr.mp3'
+import mile8 from '../sounds/movies/mile8.mp3'
+import pirates from '../sounds/movies/pirates.mp3'
+import transformers from '../sounds/movies/transformers.mp3'
+
+import angel from '../sounds/series/angel.mp3'
+import bigbang from '../sounds/series/bigbang.mp3'
+import detective from '../sounds/series/detective.mp3'
+import smallvile from '../sounds/series/smallvile.mp3'
+import supernatural from '../sounds/series/supernatural.mp3'
+import trueblood from '../sounds/series/trueblood.mp3'
+
+import animalworld from '../sounds/tvshows/animalworld.mp3'
+import director from '../sounds/tvshows/director.mp3'
+import fort from '../sounds/tvshows/fort.mp3'
+import jungle from '../sounds/tvshows/jungle.mp3'
+import pimpmyride from '../sounds/tvshows/pimpmyride.mp3'
+import xfactor from '../sounds/tvshows/xfactor.mp3'
+
+import applause from '../sounds/applause.mp3'
+import wrong from '../sounds/error.mp3'
+import right from '../sounds/right.mp3'
+
+
+const sounds = {
+    anime: {
+        alchemist: alchemist,
+        beck: beck,
+        fate: fate,
+        grand_blue: grandblue,
+        soul_eater: souleater,
+        steins_gate: steinsgate,
+    },
+    cartoons: {
+        alladin: alladin,
+        duck_tales: ducktales,
+        frozen: frozen,
+        mulan: mulan,
+        chip_and_dale: chipanddale,
+        tmnt: tmnt,
+    },
+    games: {
+        ac2: ac2,
+        battlefield: battlefield,
+        divinity: divinity,
+        fallout4: fallout4,
+        morrowind: morrowind,
+        undertale: undertale,
+    },
+    movies: {
+        badboys: badboys,
+        ghostbusters: ghostbusters,
+        lotr: lotr,
+        mile8: mile8,
+        pirates: pirates,
+        transformers: transformers,
+    },
+    series: {
+        angel: angel,
+        bigbang: bigbang,
+        detective: detective,
+        smallvile: smallvile,
+        supernatural: supernatural,
+        trueblood: trueblood,
+    },
+    tv_shows: {
+        animalworld: animalworld,
+        director: director,
+        fort: fort,
+        jungle: jungle,
+        pimpmyride: pimpmyride,
+        xfactor: xfactor,
+    },
+    etc:{
+        applause:applause,
+        right:right,
+        wrong:wrong,
+    }
+};
+
+export default sounds
